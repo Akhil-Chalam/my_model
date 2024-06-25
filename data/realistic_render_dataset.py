05bcd753ab09ba884f6da2fc95aed0f783b76a87
achalamc <akhilchalam@gmail.com>
2024-06-25 18:02:31 -0400

    bug fixes

--- data/realistic_render_dataset.py
+++ data/realistic_render_dataset.py
@@ -59,7 +59,7 @@ class realistic_render_dataset(torch.utils.data.Dataset):
         for i in range(len(real_list)):
             assert os.path.splitext(rendered_list[i])[0].split("_")[2] == os.path.splitext(mask_list[i])[0].split("_")[2], '%s and %s are not matching' % (rendered_list[i], mask_list[i])
             assert os.path.splitext(rendered_list[i])[0].split("_")[3] == os.path.splitext(mask_list[i])[0].split("_")[3], '%s and %s are not matching' % (rendered_list[i], mask_list[i])
-        return real_list, rendered_list, mask_list (path_real, path_rendered, path_mask)
+        return real_list, rendered_list, mask_list, (path_real, path_rendered, path_mask)
 
     def transforms(self, real, rendered, mask):
         assert real.size == mask.size
