e02111be9f45a7cf964c0d089b267388fd3f1dfe
achalamc <akhilchalam@gmail.com>
2024-06-26 17:35:20 -0400

    bug fixes

--- models/generator.py
+++ models/generator.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import models.norms as norms
 import torch
 import torch.nn.functional as F
+import pdb
 
 
 class Generator(nn.Module):
@@ -38,6 +39,7 @@ class Generator(nn.Module):
             z = z.expand(z.size(0), self.opt.z_dim, seg.size(2), seg.size(3))
             seg = torch.cat((z, seg), dim = 1)
         x = F.interpolate(seg, size=(self.init_W, self.init_H))
+        pdb.set_trace()
         x = self.fc(x)
         for i in range(self.opt.num_res_blocks):
             x = self.body[i](x, seg)
